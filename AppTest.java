42266aeda182896916ed0da4a0d3c7758112b3e4
atom4real <atomfaster@gmail.com>
2021-01-14 06:24:16 +0000

    Test code completed.

---
+++ AppTest.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class AppTest {
+	private App appObj;
+
+	@Before
+	public void setup(){
+		appObj = new App();
+	}
+
+	@Test
+	public void testSmallPrime(){
+		assertEquals(true, appObj.isPrime(17));
+	}
+
+	@Test
+	public void testSmallNotPrime(){
+		assertEquals(false, appObj.isPrime(33));
+	}
+
+	@Test
+	public void testLargePrime(){
+		assertEquals(true, appObj.isPrime(32416189049L));
+	}
+
+	@Test
+	public void testLargeNotPrime(){
+		assertEquals(false, appObj.isPrime(32416189051L));
+	}
+
+}
